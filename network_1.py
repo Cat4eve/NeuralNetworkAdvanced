59503b0a8a22fb10ff8194e1859327199bada7e7
Cat4eve <71460804+Cat4eve@users.noreply.github.com>
2023-05-29 18:43:31 +0400

    Create network_1.py

---
+++ network_1.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+class DenseLayer:
+    def __init__(self, input_size, output_size, activation='sigmoid'):
+        self.weights = np.random.randn(input_size, output_size) * 0.01
+        self.biases = np.zeros(output_size)
+    
+    def forward(self, inputs):
+        self.inputs = inputs
+        self.output = np.dot(inputs, self.weights) + self.biases
+        
+        return self.output
+    
+    def backward(self, grad_output, learning_rate):
+        grad_weights = np.dot(self.inputs.T, grad_output)
+        grad_biases = np.sum(grad_output, axis=0)
+        
+        grad_input = np.dot(grad_output, self.weights.T)
+        self.weights -= learning_rate * grad_weights
+        self.biases -= learning_rate * grad_biases
+        
+        return grad_input
+    
+class DenseNetwork:
+    def __init__(self):
+        self.layers = []
+    
+    def add_layer(self, layer):
+        self.layers.append(layer)
+    
+    def forward(self, inputs):
+        for layer in self.layers:
+            inputs = layer.forward(inputs)
+        return inputs
+    
+    def backward(self, grad_output, learning_rate):
+        for layer in reversed(self.layers):
+            grad_output = layer.backward(grad_output, learning_rate)
